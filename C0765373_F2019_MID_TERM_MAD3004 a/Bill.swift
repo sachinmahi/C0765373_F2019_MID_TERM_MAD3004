3f2d647a403a911a1e34503b1ddbf52ec17de64e
simarmahi <Simarmahi7696>
2019-10-19 19:02:22 -0400

    bill class created

---
+++ C0765373_F2019_MID_TERM_MAD3004 a/Bill.swift
@@ -0,0 +1,47 @@
+//
+//  Bill.swift
+//  C0765373_F2019_MID_TERM_MAD3004 a
+//
+//  Created by simar mahi on 19/10/19.
+//  Copyright © 2019 simar mahi. All rights reserved.
+//
+
+import Foundation
+
+
+class Bill : IDisplay
+{
+    
+    
+    var BillID:Int?
+    var BillDate=String()
+    enum Types {
+        case Internet
+        case Mobile
+        case Insurance
+    }
+    var BillType : Types
+    var BillAmount:Float?
+    
+    var NewDate:Date
+    {
+        let dateformatter=DateFormatter()
+        dateformatter.dateFormat="MM/DD/YYYY"
+        return dateformatter.date(from: BillDate)!
+    }
+    
+    init(BillID:Int,BillDate:String,BillType:Types,BillAmount:Float) {
+        self.BillID=BillID
+        self.BillDate=BillDate
+        self.BillType=BillType
+        self.BillAmount=BillAmount
+    }
+    func display()
+    {
+        print("Bill ID : \(BillID!)")
+        print("Bill Date : \(NewDate.insertdate())")
+        print("Bill Type : \(BillType)")
+        print("Bill Amount : \(BillAmount!.insertCurrency())")
+        
+        }
+}
