0b0ab9aab6d8ab4040ee65d8a89030b24a955ea6
simarmahi <Simarmahi7696>
2019-10-19 18:55:44 -0400

    Initial Commit

---
+++ C0765373_F2019_MID_TERM_MAD3004 a/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  C0765373_F2019_MID_TERM_MAD3004 a
+//
+//  Created by simar mahi on 19/10/19.
+//  Copyright © 2019 simar mahi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
