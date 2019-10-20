50bca6474015015b1dbe8945ae3ee414fea986f3
simarmahi <Simarmahi7696>
2019-10-19 20:37:10 -0400

    main

--- C0765373_F2019_MID_TERM_MAD3004 a/main.swift
+++ C0765373_F2019_MID_TERM_MAD3004 a/main.swift
@@ -23,7 +23,7 @@ var insurance = Insurance(BillID: 531, BillDate: "09/30/2019", BillType: .Insura
 
 
 
-var Customer = customer(CustomerID: 1, FisrtName: "SIMAR", Email: "simar.mahi@gmail.com ")
+var Customer = Customer(CustomerID: 1, FisrtName: "SIMAR", Email: "simar.mahi@gmail.com ")
 
 
 
